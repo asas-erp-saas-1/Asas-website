8ddc3290c840ec0401d01ca43c22ddeba31dd036
Asas erp saas <asas.agency.dz@gmail.com>
2026-08-21 14:39:34 +0200

    chore: remove unused transitional catalog wrapper

--- src/components/pages/ProjectsCatalogClient.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import ProjectsPage from '@/components/pages/ProjectsPage';
-import type { Project } from '@/lib/types';
-
-export default function ProjectsCatalogClient({ initialProjects }: { initialProjects: Project[] }) {
-  return <ProjectsPage initialProjects={initialProjects} />;
-}
